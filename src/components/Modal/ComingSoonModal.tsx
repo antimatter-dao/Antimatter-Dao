69c9ff39126aa024936041ada4efa2f4e765d8f5
xf <1160224892@qq.com>
2021-12-29 22:16:25 +0800

    hide availablePath

--- src/components/Modal/ComingSoonModal.tsx
+++ src/components/Modal/ComingSoonModal.tsx
@@ -28,7 +28,7 @@ const Backdrop = styled('div')({
   backdropFilter: 'blur(2px)'
 })
 
-const availablePath = [routes.stake, routes.dashboard, routes.trading_rewards]
+const availablePath = [routes.stake, routes.dashboard]
 
 export default function ComingSoonMoadal() {
   const [isOpen, setIsOpen] = useState(false)
